58a4d78ce11edf87515d974478befa9488d68bac
Kevin Malekera <r0900654@student.thomasmore.be>
2024-02-13 15:45:02 +0100

    Added JS api functions and HTML elements

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+const speakBtn = document.querySelector('#speakBtn');
+ 
+const recognition = new SpeechRecognition();
+recognition.lang = 'en-US';
+ 
+recognition.onresult = function (event) {
+  const transcript = event.results[0][0].transcript;
+  console.log(transcript);
+  document.querySelector("#output").innerHTML += transcript + "<br>";
+  speakBtn.disabled = false;
+}
+ 
+speakBtn.addEventListener("click", function () {
+  recognition.start();
+  speakBtn.disabled = true;
+});
