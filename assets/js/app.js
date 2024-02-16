132474b59181f381006d009023fff1bba8e2d18f
Kevin Malekera <r0900654@student.thomasmore.be>
2024-02-16 14:06:06 +0100

    Add functionality to hide the intro section after pressing start button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,16 @@
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-const speakBtn = document.querySelector('#speakBtn');
+
+// Btn's
+const speakBtn = document.querySelector('#speak-button');
+const startBtn = document.querySelector('#start-button');
+
+// Article's and sections
+const introSection = document.querySelector('#intro-section');
+// Question articles
+const quizQuestion1 = document.querySelector('#quiz-question-1');
  
 const recognition = new SpeechRecognition();
-recognition.lang = 'nl-BE';
+recognition.lang = 'en-US';
  
 recognition.onresult = function (event) {
   const transcript = event.results[0][0].transcript;
@@ -10,8 +18,14 @@ recognition.onresult = function (event) {
   document.querySelector('#output').innerHTML += transcript + '<br>';
   speakBtn.disabled = false;
 }
- 
+
+// EventListeners
 speakBtn.addEventListener('click', function () {
   recognition.start();
   speakBtn.disabled = true;
+});
+
+startBtn.addEventListener('click', function () {
+  introSection.classList.add('hidden');
+  quizQuestion1.classList.remove('hidden');
 });
