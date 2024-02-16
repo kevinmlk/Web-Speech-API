bca950cb207af5f4e4215b8d94b9f241dbed78d7
Kevin Malekera <r0900654@student.thomasmore.be>
2024-02-16 14:20:59 +0100

    Added question answers and stored the list items in variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,18 @@ const startBtn = document.querySelector('#start-button');
 
 // Article's and sections
 const introSection = document.querySelector('#intro-section');
+
 // Question articles
 const quizQuestion1 = document.querySelector('#quiz-question-1');
+
+// Answers container & list items
+const answersContainer = document.querySelector('.answers-container');
+const answerContainerItems = answersContainer.querySelectorAll('li');
+
+// Answers
+const answerQuestion1 = 'pragmatic dreamers';
+const answerQuestion2 = 'pragmatic dreamers';
+const answerQuestion3 = 'pragmatic dreamers';
  
 const recognition = new SpeechRecognition();
 recognition.lang = 'en-US';
